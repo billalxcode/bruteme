040de2148f65abaabef4708500c323e5d0bc5c8f
Billal06 <billal.krsz006@gmail.com>
2020-09-24 08:01:20 +0700

    Update banners

---
+++ common/banners.py
@@ -0,0 +1,14 @@
+def logo():
+    print ("""
+               /"\\
+              |\./|            Simple tools Hacking
+              |0.1|                 Linux only
+              |0.1|             Brute Force for Me
+              |>*<|  Please don't use for criminals activity
+              |0.1|           ######################
+           /'\|0.1|/'\        #  @blackcodercrush  #
+       /'\\|   |0.1|   |       #    @billalxcode    #
+      |   |   |0.1|   |\      ######################
+      |   |   |0.1|   | \\
+      | *   *   *   * |>  > copyright (c) 2020 Billal Fauzan
+""")
